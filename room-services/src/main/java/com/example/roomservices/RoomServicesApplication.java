0b9ed71dfeca6e9e38027dcc503342a78eb894dd
bkorinnyi <bohdan.korinnyi@gmail.com>
2019-10-06 13:18:52 +0300

    add swagger congig

--- room-services/src/main/java/com/example/roomservices/RoomServicesApplication.java
+++ room-services/src/main/java/com/example/roomservices/RoomServicesApplication.java
@@ -2,7 +2,16 @@ package com.example.roomservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static java.util.Collections.emptyList;
+import static springfox.documentation.builders.PathSelectors.any;
 
 @EnableSwagger2
 @SpringBootApplication
@@ -10,6 +19,21 @@ public class RoomServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoomServicesApplication.class, args);
+    }
+
+
+    @Bean
+    public Docket api() {
+        Contact contact = new Contact("Bohdan Korinnyi", null, "bohdan.korinnyi@gmail.com");
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Room")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.roomservices"))
+                .paths(any())
+                .build()
+                .apiInfo(new ApiInfo("Room Services",
+                        "A set of services to provide data access to rooms", "1.0.0", null,
+                        contact, null, null, emptyList()));
     }
 
 }
--- room-services/src/main/java/com/example/roomservices/controller/RoomController.java
+++ room-services/src/main/java/com/example/roomservices/controller/RoomController.java
@@ -2,6 +2,10 @@ package com.example.roomservices.controller;
 
 import com.example.roomservices.entity.Room;
 import com.example.roomservices.repository.RoomRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.List;
 
-@RestController
 @AllArgsConstructor
+@RestController
 @RequestMapping("rooms")
+@Api(tags = {"rooms"})
+@SwaggerDefinition(tags = {@Tag(name = "rooms", description = "Data service operations on rooms")})
 public class RoomController {
 
     private RoomRepository roomRepository;
 
     @GetMapping
+    @ApiOperation(value = "Get All Rooms", notes = "Gets all rooms in the system", nickname = "getRooms")
     public List<Room> findAllRooms(@RequestParam(required = false) String roomNumber) {
         if (StringUtils.isNoneEmpty(roomNumber)) {
             return Collections.singletonList(roomRepository.findByRoomNumber(roomNumber));
